2fc7be7cd9164ae42cf52023a4b4ff548c99238b
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-15 13:51:23 +0200

    Add project files.

---
+++ TradeManager.Service/Model/ProductTrade.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeManager.Service.Model
+{
+    public class ProductTrade
+    {
+        public Guid Id { get; private set; }
+
+        public string Name { get; set; }
+
+        public int Type { get; set; }
+
+        public string Details { get; set; }
+
+        public Guid SchemaId { get; set; }
+
+        public Guid PortfolioId { get; set; }
+
+        public Trade(string name, int type, string details, Guid schemaId, Guid portfolioId)
+        {
+            Id = new Guid();
+            Name = name;
+            Type = type;
+            Details = details;
+            SchemaId = schemaId;
+            PortfolioId = portfolioId;
+        }
+
+    }
+}
