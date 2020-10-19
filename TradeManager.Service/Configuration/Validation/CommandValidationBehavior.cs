060a0a731a642f22d0b7001c93392f6741193873
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-19 08:46:47 +0200

    added MediatorModule

---
+++ TradeManager.Service/Configuration/Validation/CommandValidationBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+
+
+// details from https://lostechies.com/jimmybogard/2016/10/13/mediatr-pipeline-examples/
+
+namespace TradeManager.Service.Configuration.Validation
+{
+    public class CommandValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IList<IValidator<TRequest>> _validators;
+
+        public CommandValidationBehavior(IList<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var errors = _validators
+                .Select(v => v.Validate(request))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (errors.Any())
+            {
+                var errorBuilder = new StringBuilder();
+
+                errorBuilder.AppendLine("error , details : ");
+
+                foreach (var error in errors)
+                {
+                    errorBuilder.AppendLine(error.ErrorMessage);
+                }
+
+                throw new InvalidCommandException(errorBuilder.ToString(), null);
+            }
+
+            return next();
+        }
+  
+    }
+}
