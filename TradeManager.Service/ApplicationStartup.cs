f8f0de4cdbd2f1c930f84a31e94d324b17150e9e
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-20 15:03:11 +0200

    complited implementation quartz

--- TradeManager.Service/ApplicationStartup.cs
+++ TradeManager.Service/ApplicationStartup.cs
@@ -4,6 +4,14 @@ using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using CommonServiceLocator;
 using Autofac.Extras.CommonServiceLocator;
+using TradeManager.Service.DomainEvents.Processing;
+using Quartz.Impl;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TradeManager.Service.SeedWork;
+using TradeManager.Infrastructure.Quartz;
+using Quartz;
+using TradeManager.Service.Processing.Events;
 
 namespace TradeManager.Service
 {
@@ -19,6 +27,8 @@ namespace TradeManager.Service
             var containerBuilder = new ContainerBuilder();
             containerBuilder.Populate(services);
 
+            containerBuilder.RegisterModule(new MediatorModule());
+            containerBuilder.RegisterModule(new ProcessingModule());
 
             // need register moduls
             // in this context the module we need is the mediator to process the internal domain commands
@@ -33,6 +43,44 @@ namespace TradeManager.Service
             CompositionRoot.SetContainer(containerBuilded);
 
             return autofacServiceProvider;
+        }
+
+        private static void StartJobScheduler()
+        {
+            var schedulerFactory = new StdSchedulerFactory();
+            var scheduler = schedulerFactory.GetScheduler().GetAwaiter().GetResult();
+
+            var container = new ContainerBuilder();
+
+            // need quartz autofac module
+            container.RegisterModule(new MediatorModule());
+            container.RegisterModule(new ProcessingModule());
+
+            container.Register(c =>
+            {
+                var dbContextOptionsBuilder = new DbContextOptionsBuilder<UpsLightContext>();
+              
+                dbContextOptionsBuilder
+                    .ReplaceService<IValueConverterSelector, StronglyTypedIdValueConverterSelector>();
+
+                return new UpsLightContext(dbContextOptionsBuilder.Options);
+            }).AsSelf().InstancePerLifetimeScope();
+
+
+            scheduler.JobFactory = new JobFactory(container.Build());
+
+            scheduler.Start().GetAwaiter().GetResult();
+
+            // https://www.quartz-scheduler.net/documentation/quartz-2.x/tutorial/crontriggers.html 
+            var processInternalCommandsJob = JobBuilder.Create<ProcessInternalCommandsJob>().Build();
+            var triggerCommandsProcessing =
+                TriggerBuilder
+                    .Create()
+                    .StartNow()
+                    .WithCronSchedule("0/5 * * ? * *")
+                    .Build();
+            scheduler.ScheduleJob(processInternalCommandsJob, triggerCommandsProcessing).GetAwaiter().GetResult();
+
         }
     }
 }
