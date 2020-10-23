cc9193974cd8e2b5ebdcf68a4f0f883f1d26f9b0
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-23 09:18:30 +0200

    Added Trade Event handler

---
+++ TradeManager.Service/DomainEvents/Trades/IntegrationHandlers/TradeRegisterEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TradeManager.Service.Domain.Trade;
+
+namespace TradeManager.Service.DomainEvents.Trades.IntegrationHandlers
+{
+  
+        public class TradeRegisteredEventHandler : INotificationHandler<TradeRegisteredEvent>
+        {
+      
+
+            public TradeRegisteredEventHandler()
+            {
+                
+            }
+
+            public async Task Handle(TradeRegisteredEvent notification, CancellationToken cancellationToken)
+            {
+
+               // handler
+
+              // opt1 call RestApi
+            }
+
+    }
+    
+}
