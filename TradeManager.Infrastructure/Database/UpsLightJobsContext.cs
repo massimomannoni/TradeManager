9ca1476115fa19e4eae1a12f35f30aca1c44e879
Massimo Mannoni <massimo.mannoni@fintech.srl>
2020-10-23 17:12:04 +0200

    Added Integration for scheduler

---
+++ TradeManager.Infrastructure/Database/UpsLightJobsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+
+namespace TradeManager.Infrastructure.Database
+{
+    public class UpsLightJobsContext : DbContext
+    {
+        //public DbSet<ProductTrade> ProductTrade { get; set; }
+
+        //public DbSet<EventStore> EventStore { get; set; }
+
+        protected UpsLightJobsContext() { }
+
+        public UpsLightJobsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
